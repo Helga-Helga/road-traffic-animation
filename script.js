66de2cdd1871d63ac482445b8f2cd256e1ac55a1
helga-helga <olyalavyagina@gmail.com>
2017-01-27 15:08:42 +0200

    Used new names for const variables

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const delimiterHeight = 4;
+const DELIMITER_HEIGHT = 4;
+const LANE_HEIGHT = 50;
 
 function draw() {
-  var canvas = document.getElementById('canvas');
+  const canvas = document.getElementById('canvas');
   if(!canvas.getContext) return;
   const ctx = canvas.getContext('2d');
 
-  const lanes = 2;
-  const laneHeight = 50;
-  const roadHeight = lanes * laneHeight + (lanes - 1) * delimiterHeight;
+  const LANES = 2;
+  const roadHeight = LANES * LANE_HEIGHT + (LANES - 1) * DELIMITER_HEIGHT;
 
   canvas.setAttribute('width', 300);
   canvas.setAttribute('height', roadHeight);
@@ -16,24 +16,26 @@ function draw() {
   road.rect(0, 0, 300, roadHeight);
   ctx.fill(road);
 
-  for (let i = 0; i < lanes + 1; i++) {
-    drawDashedPath(ctx, laneHeight + laneHeight * i + delimiterHeight * i);
+  for (let i = 0; i < LANES + 1; i++) {
+    drawDashedPath(ctx, LANE_HEIGHT + LANE_HEIGHT * i + DELIMITER_HEIGHT * i);
   }
 
-  drawCar(ctx, 10, laneHeight / 6);
+  for (let i = 0; i < LANES + 1; i++) {
+    drawCar(ctx, 0, LANE_HEIGHT / 3 + LANE_HEIGHT * i + DELIMITER_HEIGHT * i);
+  }
 }
 
 function drawDashedPath(ctx, start) {
   for (let i = 0; i < 20; i++) {
     ctx.fillStyle = 'white';
-    ctx.fillRect(i * 16, start, 8, delimiterHeight);
+    ctx.fillRect(i * 16, start, 8, DELIMITER_HEIGHT);
   }
 }
 
 function drawCar(ctx, x, y) {
  var car = new Image();
  car.onload = function() {
-   ctx.drawImage(car, x, y, 30, 15);
+   ctx.drawImage(car, x, y, 40, 20);
  };
  car.src = 'pictures/car1.png';
 }
