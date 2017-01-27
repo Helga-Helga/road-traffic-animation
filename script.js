21b8571a491c551699819be29db76f1183fd84b2
helga-helga <olyalavyagina@gmail.com>
2017-01-27 14:27:04 +0200

    Removed indents

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 function draw() {
-        var canvas = document.getElementById('canvas');
-        if (canvas.getContext){
-          var ctx = canvas.getContext('2d');
+  var canvas = document.getElementById('canvas');
+  if(!canvas.getContext) return;
+  var ctx = canvas.getContext('2d');
 
-          var lanes = 2;
-          var laneHeight = 50;
-          var roadHeight = lanes * laneHeight + (lanes - 1) * 4;
+  var lanes = 2;
+  var laneHeight = 50;
+  var roadHeight = lanes * laneHeight + (lanes - 1) * 4;
 
-          canvas.setAttribute('width', 300);
-          canvas.setAttribute('height', roadHeight);
+  canvas.setAttribute('width', 300);
+  canvas.setAttribute('height', roadHeight);
 
-          var road = new Path2D();
-          road.rect(0, 0, 300, roadHeight);
-          ctx.fill(road);
+  var road = new Path2D();
+  road.rect(0, 0, 300, roadHeight);
+  ctx.fill(road);
 
-          for (var i = 0; i < lanes + 1; i++) {
-            drawDashedPath(ctx, laneHeight + laneHeight * i + 4 * i);
-          }
+  for (var i = 0; i < lanes + 1; i++) {
+    drawDashedPath(ctx, laneHeight + laneHeight * i + 4 * i);
+  }
 
-          function drawDashedPath(ctx, start) {
-            for (var i = 0; i < 20; i++) {
-              ctx.fillStyle = 'white';
-              ctx.fillRect(i * 16, start, 8, 4);
-            }
-          }
-        }
-      }
+  function drawDashedPath(ctx, start) {
+    for (var i = 0; i < 20; i++) {
+      ctx.fillStyle = 'white';
+      ctx.fillRect(i * 16, start, 8, 4);
+    }
+  }
+}
