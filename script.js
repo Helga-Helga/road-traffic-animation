468f4a9ef4684ce523f2e01c2f59971ef1e58999
helga-helga <olyalavyagina@gmail.com>
2017-01-27 14:31:21 +0200

    Used let and const instead of var

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 function draw() {
   var canvas = document.getElementById('canvas');
   if(!canvas.getContext) return;
-  var ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext('2d');
 
-  var lanes = 2;
-  var laneHeight = 50;
-  var roadHeight = lanes * laneHeight + (lanes - 1) * 4;
+  const lanes = 2;
+  const laneHeight = 50;
+  const roadHeight = lanes * laneHeight + (lanes - 1) * 4;
 
   canvas.setAttribute('width', 300);
   canvas.setAttribute('height', roadHeight);
 
-  var road = new Path2D();
+  const road = new Path2D();
   road.rect(0, 0, 300, roadHeight);
   ctx.fill(road);
 
-  for (var i = 0; i < lanes + 1; i++) {
+  for (let i = 0; i < lanes + 1; i++) {
     drawDashedPath(ctx, laneHeight + laneHeight * i + 4 * i);
   }
 
   function drawDashedPath(ctx, start) {
-    for (var i = 0; i < 20; i++) {
+    for (let i = 0; i < 20; i++) {
       ctx.fillStyle = 'white';
       ctx.fillRect(i * 16, start, 8, 4);
     }
